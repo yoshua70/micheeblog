7e3be2067c3922eb4b9e0ee3de830baad8533918
Allidjinou Michée Josué <allidjinoumichee@outlook.fr>
2020-09-04 12:27:29 +0000

    Changes to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import './../styles/Header.css';
+import Nav from './Nav';
 
 export default class Header extends React.Component {
   render() {
     return (
       <header className="header">
         <h1 className="header__sitename">michee allidjinou</h1>
-        <nav className="header__nav">
-          <Link to="/">Accueil</Link>
-          <Link to="/about">A propos</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/blog">Blog</Link>
-        </nav>
+        <Nav/>
       </header>
     );
   }
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import './../styles/Nav.css';
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <nav className="nav">
+        <Link to="/" className="nav__link">Accueil</Link>
+        <Link to="/about" className="nav__link">A propos</Link>
+        <Link to="/contact" className="nav__link">Contact</Link>
+        <Link to="/blog" className="nav__link">Blog</Link>
+      </nav>
+    )
+  }
+}
