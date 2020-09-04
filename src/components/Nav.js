12e1ea6f5a87773aace4860f0a11a3c0fae2339a
Allidjinou Michée Josué <allidjinoumichee@outlook.fr>
2020-09-04 13:09:59 +0000

    Modify css classes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import './../styles/Nav.css';
 export default class Nav extends React.Component {
   render() {
     return (
-      <nav className="nav">
+      <nav className="nav wrapper">
         <Link to="/" className="nav__link">Accueil</Link>
         <Link to="/about" className="nav__link">A propos</Link>
         <Link to="/contact" className="nav__link">Contact</Link>
